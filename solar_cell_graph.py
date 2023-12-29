8a0e63912f7b70ad15df3f8cc34446ef0c78ab7f
Avyukt Soni <95626105+avyuktsoni0731@users.noreply.github.com>
2023-12-29 23:34:00 +0530

    Update and rename graph.py to solar_cell_graph.py

--- solar_cell_graph.py
+++ solar_cell_graph.py
@@ -2,18 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 plt.figure(figsize=(10, 6))
-df=pd.read_csv('solar_data.csv')
+df=pd.read_csv('solar_cell_data.csv')
 
 #specifying the x and y axes for plotting and sorting the data in ascending
-sdx=pd.read_csv('solar_data.csv', usecols=['Voltage']).sort_values(by='Voltage', ascending=True)
-sdy=pd.read_csv('solar_data.csv', usecols=['Current'])
+sdx=pd.read_csv('solar_cell_data.csv', usecols=['Voltage']).sort_values(by='Voltage', ascending=True)
+sdy=pd.read_csv('solar_cell_data.csv', usecols=['Current'])
 
 #plots for visual representaion
 plt.plot(sdx,sdy)
 plt.xlabel('Voltage')
 plt.ylabel('Current')
-plt.show()
-
-plt.legend()
-plt.title('Solar Cell I-V characteristics')
 plt.show()
