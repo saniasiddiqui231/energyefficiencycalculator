677df6d24f2908e4873452b2b2d6d47671c863df
Avyukt Soni <95626105+avyuktsoni0731@users.noreply.github.com>
2023-12-31 22:09:08 +0530

    Update main.py

--- wind_turbine_as/main.py
+++ wind_turbine_as/main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import csv
 
 # reading the csv file
 
@@ -7,14 +6,20 @@ data = pd.read_csv("input.csv")
 w_speed=pd.read_csv('input.csv', usecols=['Wind speed m/s'])
 p_out=pd.read_csv('input.csv', usecols=['output(kW)'])
 
+# making user input the rotor radius
+
 rotor_rad = float(input("Enter the rotor radius (more than 17 meters): "))
 eff_data = []
+
+# defining the function to calculate the efficiency of the wind turbine
 
 def calculate_efficiency(power_out):
       efficiency = round((power_out / power_input) * 100, 2)
       eff_data.append(efficiency)
 
 k=len(data)
+
+# loop to calculate the input power
 
 for i in range(0,k):
       wind_speed = w_speed.loc[i].item()
@@ -24,7 +29,9 @@ for i in range(0,k):
       else:
             power_input = 0.5*3.14*1.204*rotor_rad*wind_speed
             calculate_efficiency(power_out)
-      
+
+# storing values of efficiency into a csv file
+
 dict = {'efficiency': eff_data}
 df = pd.DataFrame(dict)
 df.to_csv('output.csv')
