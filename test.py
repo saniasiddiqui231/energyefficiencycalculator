fad47c78ee0bcc2f3ed30600fc2bbed0e11c962b
Avyukt Soni <soniavyukt@gmail.com>
2024-01-31 22:36:19 +0530

    testing firebase

--- test.py
+++ test.py
@@ -1,25 +1,21 @@
-### firebase
 import pyrebase
 
-config = {
-    "apiKey": "AIzaSyB7P2TBuIvdKqDx8ovsOy9phB4KvUpULH0",
-    "authDomain": "efficiency-calculator.firebaseapp.com",
-    "projectId": "efficiency-calculator",
-    "storageBucket": "efficiency-calculator.appspot.com",
-    "messagingSenderId": "1037551695369",
-    "appId": "1:1037551695369:web:d27c3d72cbf583aaadaad8",
-    "measurementId": "G-9QYEF7L9Q1"
+firebaseConfig = {
+    'apiKey': "AIzaSyB7P2TBuIvdKqDx8ovsOy9phB4KvUpULH0",
+    'authDomain': "efficiency-calculator.firebaseapp.com",
+    'projectId': "efficiency-calculator",
+    'storageBucket': "efficiency-calculator.appspot.com",
+    'messagingSenderId': "1037551695369",
+    'appId': "1:1037551695369:web:d27c3d72cbf583aaadaad8",
+    'measurementId': "G-9QYEF7L9Q1"
 }
 
-firebase = pyrebase.initialize_app(config)
+firebase = pyrebase.initialize_app(firebaseConfig)
 
-auth = firebase.auth()
+# setting up storage
+storage = firebase.storage()
 
-email = input("Enter your email: ")
-password = input("Enter your password: ")
-
-user = auth.create_user_with_email_and_password(email, password)
-
-auth.get_account_info(user['idToken'])
-
-### firebase
+# upload files to storage
+file = input("Enter the filename: ")
+cloudfilename = input("Enter the cloud filename: ")
+storage.child(cloudfilename).put(file)
